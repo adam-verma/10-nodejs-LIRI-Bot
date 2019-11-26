b5bd8d4da29a5f28fe7068bb74f1b9f3d73bd0be
Adam Verma <vermaadam@gmail.com>
2019-11-25 19:10:03 -0600

    Spotify, OMDB, and Bands in Town API Call Functions (Spotify not running yet), Switch-Case for Functions

--- assets/js/liri.js
+++ assets/js/liri.js
@@ -14,12 +14,33 @@ let bandsintown = keys.bandsintown;
 let omdb = keys.omdb; 
 
 // Create an argument line from user 
-userInput = process.argv[2]; 
+let userInput = process.argv[2]; 
 
-// Create functions returning these commands: concert-this, spotify-this-song, movie-this, do-what-it-says
+// Create function returning these commands: concert-this, spotify-this-song, movie-this, do-what-it-says
+function action(userInput) { 
+    // Apply a switch-case to apply the input to a command
+    switch(userInput) { 
+        case "spotify-this-song":
+        spotifyThis();
+        break;
+        
+        case "concert-this": 
+        concertThis(); 
+        break;
 
+        case "movie-this": 
+        movieThis(); 
+        break; 
+
+        default:
+        console.log("Please try again with a different input.")
+    }
+}
+action(userInput);
+
+// Write a Spotify function to apply in a switch-case 
+function spotifyThis() { 
 // This runs a request with node-spotify-api to URL with specified parameters
-
 spotify
   .search({ type: 'track', query: userInput, limit: 1}
   , function (err, data) { 
@@ -29,8 +50,10 @@ spotify
     console.log(data); 
     }
   })
+}
 
-
+// Write a OMDB function to apply in a switch-case 
+function movieThis() {
 // Then run a request with axios to the OMDB API with the movie specified
 axios.get(`http://www.omdbapi.com/?t=${userInput}&y=&plot=short&apikey=${omdb}`)
 .then(function(response) {
@@ -56,4 +79,33 @@ axios.get(`http://www.omdbapi.com/?t=${userInput}&y=&plot=short&apikey=${omdb}`)
     }
     console.log(error.config);
   });
+} 
 
+// Write a Bands in Town function to apply in a switch-case
+function concertThis() { 
+// Then run a request with axios to the Bands in Town API with the artist specified
+axios.get(`https://rest.bandsintown.com/artists/" + ${userInput} + "/events?app_id=${bandsintown}`)
+.then(function(response) {
+    console.log(response); 
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
+} 
